ad715c37ec432f530f582659c89246d17e74cfda
Zohan-27 <81106911+Zohan-27@users.noreply.github.com>
2023-09-11 17:12:50 +0300

    Update LineLengthCalculator.java

--- src/LineLengthCalculator.java
+++ src/LineLengthCalculator.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -20,48 +19,54 @@ class Point {
 
 public class LineLengthCalculator {
     public static void main(String[] args) {
-        ArrayList<Point> points = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        boolean shouldEnd = false;
+        System.out.print("Введите количество точек: ");
+        int numberOfPoints = scanner.nextInt();
 
-        while (!shouldEnd) {
-            try {
-                System.out.print("Введите x координату точки (или 'exit' для завершения ввода): ");
-                String input = scanner.next();
+        if (numberOfPoints < 2) {
+            System.out.println("Недостаточно точек для вычисления длин линий.");
+            return;
+        }
 
-                if (input.equalsIgnoreCase("exit")) {
-                    break;
+        Point[] points = new Point[numberOfPoints];
+
+        try {
+            for (int i = 0; i < numberOfPoints; i++) {
+                System.out.print("Введите x координату точки " + (i + 1) + ": ");
+                String xInput = scanner.next();
+
+                if (xInput.equalsIgnoreCase("exit")) {
+                    System.out.println("Введено недопустимое значение для x.");
+                    return;
                 }
 
-                int x = Integer.parseInt(input);
+                int x = Integer.parseInt(xInput);
 
-                System.out.print("Введите y координату точки (или 'exit' для завершения ввода): ");
-                input = scanner.next();
+                System.out.print("Введите y координату точки " + (i + 1) + ": ");
+                String yInput = scanner.next();
 
-                if (input.equalsIgnoreCase("exit")) {
-                    shouldEnd = true;
-                    break;
+                if (yInput.equalsIgnoreCase("exit")) {
+                    System.out.println("Введено недопустимое значение для y.");
+                    return;
                 }
 
-                int y = Integer.parseInt(input);
+                int y = Integer.parseInt(yInput);
 
-                points.add(new Point(x, y));
-            } catch (NumberFormatException e) {
-                System.out.println("Ошибка: Введите целое число для координаты или 'exit' для завершения ввода.");
-            } catch (InputMismatchException e) {
-                System.out.println("Ошибка: Введите целое число для координаты.");
-                scanner.nextLine(); // Очистить буфер ввода
+                points[i] = new Point(x, y);
             }
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка: Введите целое число для координаты.");
+            return;
         }
 
         double minLength = Double.MAX_VALUE;
         double maxLength = Double.MIN_VALUE;
         double totalLength = 0;
 
-        for (int i = 0; i < points.size() - 1; i++) {
-            Point p1 = points.get(i);
-            Point p2 = points.get(i + 1);
+        for (int i = 0; i < numberOfPoints - 1; i++) {
+            Point p1 = points[i];
+            Point p2 = points[i + 1];
             double length = p1.distanceTo(p2);
 
             totalLength += length;
@@ -75,12 +80,8 @@ public class LineLengthCalculator {
             }
         }
 
-        if (points.size() < 2) {
-            System.out.println("Недостаточно точек для вычисления длин линий.");
-        } else {
-            System.out.println("Минимальная длина линии: " + minLength);
-            System.out.println("Максимальная длина линии: " + maxLength);
-            System.out.println("Сумма длин всех линий: " + totalLength);
-        }
+        System.out.println("Минимальная длина линии: " + minLength);
+        System.out.println("Максимальная длина линии: " + maxLength);
+        System.out.println("Сумма длин всех линий: " + totalLength);
     }
 }
